652b35cb27bfff93c5dba67fc605cf7d5e525b19
AkshanshChauhan17 <akshanshchauhan47@gmail.com>
2024-01-03 01:18:28 +0530

    v1.3

---
+++ db/face-detection-db.sql
@@ -0,0 +1,59 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jan 02, 2024 at 08:17 PM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `face_detection_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `face`
+--
+
+CREATE TABLE `face` (
+  `id` int(200) NOT NULL,
+  `image_base64` longtext NOT NULL,
+  `time_stamp` timestamp(6) NOT NULL DEFAULT current_timestamp(6) ON UPDATE current_timestamp(6)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `face`
+--
+ALTER TABLE `face`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `face`
+--
+ALTER TABLE `face`
+  MODIFY `id` int(200) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ php/save_image.php
@@ -0,0 +1,58 @@
+<?php
+    header("Content-Type: application/json");
+    
+    function GET_IMAGES() {
+    
+        $connection = new mysqli("localhost", "root", "", "face_detection_db");
+
+        if($connection->connect_error) {
+            die("Connection failed: " . $connection->connect_error);
+        }
+
+        $get_image_data_sql = $connection->query("SELECT * FROM face");
+        
+        if($get_image_data_sql->num_rows > 0) {
+            $data = array();
+
+            while ($row = $get_image_data_sql->fetch_assoc()) {
+                $data[] = $row;
+            }
+
+            echo json_encode($data);
+        } else {
+            echo json_encode(["MESSAGE"=>"DATA NOT FOUND"]);
+        }
+
+        $connection->close();
+    }
+
+    function POST_IMAGE($image_data) {
+
+        $connection = new mysqli("localhost", "root", "", "face_detection_db");
+
+        if($connection->connect_error) {
+            die("Connection failed: " . $connection->connect_error);
+        }
+
+        if($connection->query("INSERT INTO face (image_base64) VALUES ('$image_data')") === TRUE) {
+            echo json_encode(["MESSAGE"=>"DATA SUCCESSFULLY UPLOADED"]);
+        } else {
+            echo json_encode(["ERROR"=>"ERROR: " . $connection->error]);
+        }
+
+        $connection->close();
+    }
+
+    switch($_SERVER['REQUEST_METHOD']) {
+        case "GET":
+            GET_IMAGES();
+            break;
+        case "POST":
+            POST_IMAGE($_POST['img_data']);
+            break;
+        default:
+            http_response_code(404);
+            echo json_encode(["MESSAGE"=>"ONLY GET AND POST REQUESTS ALLOWED"]);
+    }
+
+?>
